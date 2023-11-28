b91e15bd8c4ef64e9278c95c0fec9e08f5be5a8f
aaditya2200 <aaditya2200@gmail.com>
2023-11-27 21:54:48 -0700

    Trying to measure latency

--- odlib/include/general_util/od_top.h
+++ odlib/include/general_util/od_top.h
@@ -107,7 +107,7 @@ typedef struct key mica_key_t;
 #define MACHINE_THAT_SLEEPS 1
 
 //! todo: maybe this is why we cant see the metrics
-#define MEASURE_LATENCY 0
+#define MEASURE_LATENCY 1
 #define LATENCY_MACHINE 0
 #define LATENCY_THREAD 0
 #define MEASURE_READ_LATENCY 2 // 2 means mixed
