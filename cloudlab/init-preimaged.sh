d3b223c606ebdae342402cd21125653680ea8db2
aaditya2200 <aaditya2200@gmail.com>
2023-12-07 16:50:22 -0700

    Added build files for splinterDB and B+-tree

--- cloudlab/init-preimaged.sh
+++ cloudlab/init-preimaged.sh
@@ -102,3 +102,33 @@ if [[ "${HOSTNAME:5:1}" == 1 ]]; then
       ssh-keyscan -H 10.0.3.${i} >> ~/.ssh/known_hosts
     done
 fi
+
+# The below lines build the .so files required for running Odyssey with B+-tree and SplinterDB
+splinterdb_folder="../splinterdb/"
+export COMPILER=gcc
+sudo apt update -y
+sudo apt install -y libaio-dev libconfig-dev libxxhash-dev $COMPILER
+export CC=$COMPILER
+export LD=$COMPILER
+cd $splinterdb_folder
+make VERBOSE=1
+cd "-"
+
+# For B+-tree
+bplus_folder="../bplus-tree/"
+cd $bplus_folder
+CC=gcc
+CSTDFLAG="--std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter"
+CPPFLAGS="-c -fpic -Iinclude -Iexternal/snappy"
+CPPFLAGS+=" -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=500 -D_DARWIN_C_SOURCE"
+LDFLAGS="-lpthread"
+SRC_DIR=src
+INCLUDE_DIR=include
+OBJ_DIR=obj
+
+for file in "$SRC_DIR"/*.c; do
+    base_name=$(basename "$file" .c)
+    $CC $CSTDFLAG $CPPFLAGS $LDFLAGS -I"$INCLUDE_DIR" "$file" -o "$OBJ_DIR/$base_name.o"
+done
+
+$CC -shared -o libbplustree.so "$OBJ_DIR"/*.o
